c4dba28db35658ce50ceca61733b04cef6ed497d
Andriamanitra <mikko77792@gmail.com>
2025-07-29 20:01:45 +0300

    handle panics more gracefully

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,42 @@
+use std::io::stdout;
+
 use bad_editor::bad;
+use crossterm::ExecutableCommand;
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::cursor::Hide as HideCursor;
+use crossterm::cursor::Show as ShowCursor;
+
+struct TerminalGuard;
+impl TerminalGuard {
+    fn acquire() -> Result<Self, Box<dyn std::error::Error>> {
+        crossterm::terminal::enable_raw_mode()?;
+        Ok(Self)
+    }
+}
+impl Drop for TerminalGuard {
+    fn drop(&mut self) {
+        let _ = crossterm::terminal::disable_raw_mode();
+        let _ = stdout().execute(ShowCursor);
+    }
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // TODO: CLI
     let app = bad::App::new();
 
-    crossterm::terminal::enable_raw_mode()?;
-    let result = app.run(&mut std::io::stdout());
-    crossterm::terminal::disable_raw_mode()?;
+    // TerminalGuard ensures raw mode gets disabled if the app crashes.
+    // Drop runs when variable leaves the scope, even on panic.
+    let terminal_guard = TerminalGuard::acquire()?;
+    stdout().execute(HideCursor)?;
+    stdout().execute(EnterAlternateScreen)?;
 
-    result
+    app.run(&mut stdout())?;
+
+    drop(terminal_guard);
+
+    // the backtrace from panicking is in the alternate screen
+    // so we only want to execute this when exiting normally
+    stdout().execute(LeaveAlternateScreen)?;
+
+    Ok(())
 }
--- src/run.rs
+++ src/run.rs
@@ -2,10 +2,6 @@ use std::error::Error;
 use std::time::{Duration, Instant};
 
 use crossterm::event::{self, KeyCode, KeyEvent, KeyModifiers};
-use crossterm::ExecutableCommand;
-use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
-use crossterm::cursor::Hide as HideCursor;
-use crossterm::cursor::Show as ShowCursor;
 
 use crate::bad::App;
 use crate::{Action, PaneAction, MoveTarget};
@@ -18,19 +14,7 @@ enum AfterActions {
 }
 
 impl App {
-    pub fn run(mut self, out: &mut dyn std::io::Write) -> Result<(), Box<dyn Error>> {
-        out.execute(EnterAlternateScreen)?;
-        out.execute(HideCursor)?;
-
-        let result = self.enter_event_loop(out);
-
-        let _ = out.execute(LeaveAlternateScreen);
-        let _ = out.execute(ShowCursor);
-
-        result
-    }
-
-    fn enter_event_loop(&mut self, mut out: &mut dyn std::io::Write) -> Result<(), Box<dyn Error>> {
+    pub fn run(mut self, mut out: &mut dyn std::io::Write) -> Result<(), Box<dyn Error>> {
         const POLL_TIMEOUT: Duration = Duration::from_millis(16);
 
         let mut need_to_render = true;
