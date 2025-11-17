dc7c98e21d5a54b0cf2e0d59f50d91a8620ea4b5
Andriamanitra <mikko77792@gmail.com>
2025-11-17 23:47:10 +0200

    use termux-api for clipboard on Android

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,7 +1,12 @@
 use std::error::Error;
 use std::fmt::Display;
 
-use crossterm::clipboard::CopyToClipboard;
+pub trait Clipboard {
+    fn new() -> Self;
+    fn copy(&mut self, content: Vec<String>) -> Result<(), ClipboardError>;
+    fn update_from_external(&mut self) -> Result<(), ClipboardError>;
+    fn content(&self) -> &[String];
+}
 
 #[derive(Debug)]
 pub enum ClipboardError {
@@ -11,9 +16,12 @@ pub enum ClipboardError {
     ConversionFailure,
     Unknown { description: String }
 }
+
+impl Error for ClipboardError {}
+
 impl Display for ClipboardError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "clipboard error: {}",
+        write!(f, "clipboard error: {}", 
             match self {
                 ClipboardError::ContentNotAvailable => "content from external clipboard not available",
                 ClipboardError::NotSupported => "external clipboard not supported",
@@ -25,62 +33,126 @@ impl Display for ClipboardError {
     }
 }
 
-impl From<arboard::Error> for ClipboardError {
-    fn from(value: arboard::Error) -> Self {
-        match value {
-            arboard::Error::ContentNotAvailable => ClipboardError::ContentNotAvailable,
-            arboard::Error::ClipboardNotSupported => ClipboardError::NotSupported,
-            arboard::Error::ClipboardOccupied => ClipboardError::Occupied,
-            arboard::Error::ConversionFailure => ClipboardError::ConversionFailure,
-            arboard::Error::Unknown { description } => ClipboardError::Unknown { description },
-            _ => ClipboardError::Unknown { description: "not sure what happened, sorry :(".into() },
+#[cfg(not(target_os = "android"))]
+pub mod arboard {
+    use crate::clipboard::Clipboard;
+    use super::ClipboardError;
+
+    impl From<arboard::Error> for ClipboardError {
+        fn from(value: arboard::Error) -> Self {
+            match value {
+                arboard::Error::ContentNotAvailable => ClipboardError::ContentNotAvailable,
+                arboard::Error::ClipboardNotSupported => ClipboardError::NotSupported,
+                arboard::Error::ClipboardOccupied => ClipboardError::Occupied,
+                arboard::Error::ConversionFailure => ClipboardError::ConversionFailure,
+                arboard::Error::Unknown { description } => ClipboardError::Unknown { description },
+                _ => ClipboardError::Unknown { description: "not sure what happened, sorry :(".into() },
+            }
+        }
+    }
+
+    pub struct ArboardClipboard {
+        /// stores one String per cursor to enable multicursor paste
+        clips: Vec<String>,
+        external_clipboard: Result<arboard::Clipboard, arboard::Error>
+    }
+
+    impl ArboardClipboard {
+        fn external(&mut self) -> Result<&mut arboard::Clipboard, ClipboardError> {
+            self.external_clipboard.as_mut().map_err(|_| ClipboardError::NotSupported)
+        }
+    }
+
+    impl Clipboard for ArboardClipboard {
+        fn new() -> Self {
+            Self {
+                clips: vec![],
+                external_clipboard: arboard::Clipboard::new(),
+            }
+        }
+
+        fn copy(&mut self, content: Vec<String>) -> Result<(), ClipboardError> {
+            self.clips = content;
+            let s = self.clips.join("\n");
+            self.external()?.set_text(&s).map_err(ClipboardError::from)
+        }
+
+        fn update_from_external(&mut self) -> Result<(), ClipboardError> {
+            let txt = self.external()?.get_text().map_err(ClipboardError::from)?;
+            if txt != self.clips.join("\n") {
+                self.clips = vec![txt];
+            }
+            Ok(())
+        }
+
+        fn content(&self) -> &[String] {
+            &self.clips
         }
     }
 }
 
-impl Error for ClipboardError {}
+// termux module is currently only used on Android builds
+#[allow(dead_code)]
+pub mod termux {
+    use crate::clipboard::Clipboard;
+    use crate::clipboard::ClipboardError;
 
-pub struct Clipboard {
-    /// stores one String per cursor to enable multicursor paste
-    clips: Vec<String>,
-    external_clipboard: Result<arboard::Clipboard, arboard::Error>
-}
+    pub struct TermuxClipboard {
+        clips: Vec<String>
+    }
 
-impl Clipboard {
-    pub fn new() -> Self {
-        Self {
-            clips: vec![],
-            external_clipboard: arboard::Clipboard::new(),
+    impl Clipboard for TermuxClipboard {
+        fn new() -> Self {
+            Self { clips: vec![] }
         }
-    }
 
-    fn external(&mut self) -> Result<&mut arboard::Clipboard, ClipboardError> {
-        self.external_clipboard.as_mut().map_err(|_| ClipboardError::NotSupported)
-    }
-
-    pub fn copy(&mut self, content: Vec<String>) -> Result<(), ClipboardError> {
-        self.clips = content;
-        let s = self.clips.join("\n");
-        // also copy to OSC52 clipboard to give the clip a better chance
-        // of surviving the application exiting under wayland
-        // https://whynothugo.nl/journal/2022/10/21/how-the-clipboard-works/
-        let _ = crossterm::execute!(
-            std::io::stdout(),
-            CopyToClipboard::to_clipboard_from(&s),
-            CopyToClipboard::to_primary_from(&s),
-        );
-        self.external()?.set_text(&s).map_err(ClipboardError::from)
-    }
-
-    pub fn update_from_external(&mut self) -> Result<(), ClipboardError> {
-        let txt = self.external()?.get_text().map_err(ClipboardError::from)?;
-        if txt != self.clips.join("\n") {
-            self.clips = vec![txt];
+        fn copy(&mut self, content: Vec<String>) -> Result<(), ClipboardError> {
+            self.clips = content;
+            let s = self.clips.join("\n");
+            let status = std::process::Command::new("termux-clipboard-set")
+                .arg(&s)
+                .status()
+                .map_err(|_| ClipboardError::Unknown {
+                    description: "unable to run termux-clipboard-get (is termux-api installed?)".to_string()
+                })?;
+            match status.code() {
+                Some(0) => Ok(()),
+                Some(code) => Err(ClipboardError::Unknown {
+                    description: format!("termux-clipboard-set exited with {code}")
+                }),
+                None => Err(ClipboardError::Unknown {
+                    description: "termux-clipboard-set was terminated by a signal".to_string()
+                }),
+            }
         }
-        Ok(())
-    }
 
-    pub fn content(&self) -> &[String] {
-        &self.clips
+        fn update_from_external(&mut self) -> Result<(), ClipboardError> {
+            let out = std::process::Command::new("termux-clipboard-get")
+                .output()
+                .map_err(|_| ClipboardError::Unknown {
+                    description: "unable to run termux-clipboard-get (is termux-api installed?)".to_string()
+                })?;
+            match out.status.code() {
+                Some(0) => {
+                    match String::from_utf8(out.stdout) {
+                        Ok(txt) => {
+                            self.clips = vec![txt];
+                            Ok(())
+                        }
+                        Err(_) => Err(ClipboardError::ConversionFailure),
+                    }
+                },
+                Some(code) => Err(ClipboardError::Unknown {
+                    description: format!("termux-clipboard-get exited with {code}")
+                }),
+                None => Err(ClipboardError::Unknown {
+                    description: "termux-clipboard-set was terminated by a signal".to_string()
+                }),
+            }
+        }
+
+        fn content(&self) -> &[String] {
+            &self.clips
+        }
     }
 }
