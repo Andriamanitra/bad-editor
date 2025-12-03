a3ec756d3bac1db7827bcc1dd402c4f1b0edaa24
Andriamanitra <mikko77792@gmail.com>
2025-12-03 21:10:00 +0200

    bring back copying to OSC52

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,6 +1,8 @@
 use std::error::Error;
 use std::fmt::Display;
 
+use crossterm::clipboard::CopyToClipboard;
+
 #[derive(Debug)]
 pub enum ClipboardError {
     ContentNotAvailable,
@@ -11,7 +13,7 @@ pub enum ClipboardError {
 }
 impl Display for ClipboardError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "clipboard error: {}", 
+        write!(f, "clipboard error: {}",
             match self {
                 ClipboardError::ContentNotAvailable => "content from external clipboard not available",
                 ClipboardError::NotSupported => "external clipboard not supported",
@@ -59,6 +61,14 @@ impl Clipboard {
     pub fn copy(&mut self, content: Vec<String>) -> Result<(), ClipboardError> {
         self.clips = content;
         let s = self.clips.join("\n");
+        // also copy to OSC52 clipboard to give the clip a better chance
+        // of surviving the application exiting under wayland
+        // https://whynothugo.nl/journal/2022/10/21/how-the-clipboard-works/
+        let _ = crossterm::execute!(
+            std::io::stdout(),
+            CopyToClipboard::to_clipboard_from(&s),
+            CopyToClipboard::to_primary_from(&s),
+        );
         self.external()?.set_text(&s).map_err(ClipboardError::from)
     }
 
