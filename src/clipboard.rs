211afb7e2499b7ec9fb4d137c87a9e97c4eefd4b
Andriamanitra <mikko77792@gmail.com>
2026-03-27 16:38:03 +0200

    copying to different clipboards separately seems to be more reliable

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,5 +1,5 @@
 use std::default::Default;
-use crossterm::clipboard::{ClipboardSelection, CopyToClipboard, ClipboardType};
+use crossterm::clipboard::CopyToClipboard;
 
 #[derive(Default)]
 pub struct InternalClipboard {
@@ -14,8 +14,8 @@ impl InternalClipboard {
     pub fn copy(&mut self, content: Vec<String>) {
         self.clips = content;
         let content = self.content().join("\n");
-        let destination = ClipboardSelection(vec![ClipboardType::Clipboard, ClipboardType::Primary]);
-        let _ = crossterm::execute!(std::io::stdout(), CopyToClipboard { content, destination });
+        let _ = crossterm::execute!(std::io::stdout(), CopyToClipboard::to_clipboard_from(&content));
+        let _ = crossterm::execute!(std::io::stdout(), CopyToClipboard::to_primary_from(&content));
     }
 
     pub fn content(&self) -> &[String] {
